f202f899ddaa10181730569e32e4ab1126479832
Anthony Kim <tony@anthonys-mbp.lan>
2019-08-17 02:07:05 -0700

    [fix] changed response message

--- server/index.js
+++ server/index.js
@@ -9,19 +9,19 @@
   server.use('/', express.static(path.join(__dirname + '/../client/dist')));
   
   server.get('/name', (req, res) => {
-    res.status(200).send('This is your get request')
+    res.status(200).send('This is your get request, modify this file to use your router!')
   })
   
   server.post('/name', (req, res) => {
-    res.status(200).send('This is your post request')
+    res.status(200).send('This is your post request, modify this file to use your router!')
   })
   
   server.put('/name', (req, res) => {
-    res.status(200).send('This is your put request')
+    res.status(200).send('This is your put request, modify this file to use your router!')
   })
   
   server.delete('/name', (req, res) => {
-    res.status(200).send('This is your delete request')
+    res.status(200).send('This is your delete request, modify this file to use your router!')
   })
 
   server.listen(port, () => console.log('Connected to port: 3000'))
